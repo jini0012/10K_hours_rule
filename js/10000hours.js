6c5ed1f2cf0eb45e37506da9bac96a7ece68ac4c
jini <jinhui.ha1208@gmail.com>
2024-10-11 14:58:59 +0900

    모달창 JS 수정

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,1 +1,11 @@
-// js file
+const dialog = document.querySelector("dialog");
+const showDialogBtn = document.getElementById("showDialogBtn");
+const closeBtn = dialog.querySelector("button");
+
+showDialogBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  dialog.close();
+});
