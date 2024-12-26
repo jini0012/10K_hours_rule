e5895837456386816911b405b20405499a10c9be
jini0012 <jinhui.ha1208@gmail.com>
2024-12-26 13:34:17 +0900

    🎨 훈련일수 계산 기능 구현

--- js/10000hours.js
+++ js/10000hours.js
@@ -9,3 +9,26 @@ showDialogBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   dialog.close();
 });
+
+const timeCalc = document.querySelector(".time-calc form");
+const viewMyGoal = document.querySelector(".view-goal");
+const viewCalcTime = document.querySelector(".view-calc-time");
+
+timeCalc.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const myGoal = timeCalc.querySelector("#job").value;
+  const myTrainingTime = timeCalc.querySelector("#hour").value;
+
+  if (!myGoal && !myTrainingTime) {
+    alert("목표와 훈련 시간을 입력하지 않았습니다!");
+  } else if (!myGoal) {
+    alert("목표를 입력하지 않았습니다!");
+  } else if (!myTrainingTime) {
+    alert("훈련시간을 입력하지 않았습니다!");
+  }
+
+  if (myGoal && myTrainingTime) {
+    viewMyGoal.textContent = myGoal;
+    viewCalcTime.textContent = Math.trunc(10000 / myTrainingTime);
+  }
+});
